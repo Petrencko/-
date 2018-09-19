30762fac7f6d431e862f59708bfcdbc9b172c5ad
Petrencko <39306668+Petrencko@users.noreply.github.com>
2018-09-19 20:14:53 +0300

    Add files via upload

---
+++ js7.js
@@ -0,0 +1,19 @@
+﻿function $()
+{ var elements = new Array();
+for (var i = 0; i < arguments.length; i++)
+{ var element = arguments[i];
+if (typeof element == 'string')
+element = document.getElementById(element);
+if (arguments.length == 1)
+return element;
+elements.push(element);
+}
+return elements; }
+function f1()
+{A=1, k=1 
+while (k<=2)
+{A=(A+1)/k;
+k=k+1}
+$('t2').value=A
+}
+
