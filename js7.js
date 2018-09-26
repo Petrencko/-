48c0ad881788efc50644a85053118d6e826da895
Petrencko <39306668+Petrencko@users.noreply.github.com>
2018-09-26 21:10:05 +0300

    Update js7.js

--- js7.js
+++ js7.js
@@ -1,22 +1,23 @@
-﻿function $()
-{ var elements = new Array();
-for (var i = 0; i < arguments.length; i++)
-{ var element = arguments[i];
-if (typeof element == 'string')
-element = document.getElementById(element);
-if (arguments.length == 1)
-return element;
-elements.push(element);
-}
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
 return elements; }
+
 function f1()
-  N=parseFloat($('t3').value);
-A=1, k=1
- for(k=1;k<=N;k++)
 { 
-while (k<=N, 
-{A=(A+1)/k;
-k=k+1}
-$('t2').value=A;
+	N=parseFloat($('t3').value);
+A=1; k=1;
+for(k=1;k<=N;k++)
+{
+S=(A+1)/k;
+}
+$('t2').value= S;
 }
 
