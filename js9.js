27f00943fb47b630a545d0329db8943a9223e7e2
Petrencko <39306668+Petrencko@users.noreply.github.com>
2018-09-26 20:25:33 +0300

    Add files via upload

---
+++ js9.js
@@ -0,0 +1,24 @@
+﻿function $()
+{ var elements = new Array();
+for (var i = 0; i < arguments.length; i++)
+{ var element = arguments[i];
+if (typeof element == 'string')
+element = document.getElementById(element);
+if (arguments.length == 1)
+return element;
+elements.push(element);
+}
+return elements; }
+function f1()
+{            
+var A;
+var B;
+
+A=parseInt($('t1').value);
+B=parseInt($('t2').value);
+A<B
+while ()
+{Z=;
+}
+$('t4').value=Z;
+}
