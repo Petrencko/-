a540eda908ddf0421ced2b7d2eca0a77daf04b47
Petrencko <39306668+Petrencko@users.noreply.github.com>
2018-09-29 18:24:51 +0300

    Update js8.js

--- js8.js
+++ js8.js
@@ -1,23 +1,26 @@
-﻿function $()
-{ var elements = new Array();
-for (var i = 0; i < arguments.length; i++)
-{ var element = arguments[i];
-if (typeof element == 'string')
-element = document.getElementById(element);
-if (arguments.length == 1)
-return element;
-elements.push(element);
-}
-return elements; }
+function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }  
+
+
 function f1()
-{
-N=parseFloat($('t1').value);
-K=parseFloat($('t2').value);
-Sum=1
-for (i=2; i<=N; i++;)
-{a=i;
-for(j=2; j<=K; j++;)
-{a=a*i;
-Sum=Sum+a;}
-$('t4').value=Sum;
-}
+{N=parseFloat($('t1').value);
+ K=parseFloat($('t2').value);
+ Sum=1;
+ 
+                     for(i=2;i<=N;i++) 
+		       {a=i;
+                           for(j=2;j<=K;j++)
+                       {a=a*i;
+                        Sum=Sum+a;}
+		  	$('t3').value=Sum;
+                                
+		             }
