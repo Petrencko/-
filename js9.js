1568e24bee5c5b7df0b696ea2f7623a341516e70
Petrencko <39306668+Petrencko@users.noreply.github.com>
2018-10-02 18:26:40 +0300

    Update js9.js

--- js9.js
+++ js9.js
@@ -1,25 +1,30 @@
-﻿function $()
-{ var elements = new Array();
-for (var i = 0; i < arguments.length; i++)
-{ var element = arguments[i];
-if (typeof element == 'string')
-element = document.getElementById(element);
-if (arguments.length == 1)
-return element;
-elements.push(element);
-}
-return elements; }
+function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }  
+
+
 function f1()
-{A=parseFloat($('t1').value);
- B=parseFloat($('t2').value);
+{N=parseFloat($('t1').value);
+ K=parseFloat($('t2').value);
+ Sum=1;
  
-                     for(i=A;i<=B;i++) 
-		       
-                     { 
-                      for(j=0;j<i;j++)
-                     { $('t3').value=i+" ";
-		  	}
-                        
+                     for(i=2;i<=N;i++) 
+		       {a=i;
+                           for(j=2;j<=K;j++)
+                       {a=a*i;
+                        Sum=Sum+a;}
+		  	$('t3').value=Sum;
+                                
+		             }
+
 }
 
 
