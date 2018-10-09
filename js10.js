cc4ad7ad0332ee1087736d1ea5cb596ff6c3c7fc
Petrencko <39306668+Petrencko@users.noreply.github.com>
2018-10-09 18:43:51 +0300

    Update js10.js

--- js10.js
+++ js10.js
@@ -12,7 +12,7 @@ return elements; }
 
 function f1()
 {N=parseFloat($('t1').value);
-                       K=1;
+                       K=0;
                 while(1+2+3+...+K<=N)
 		       K++;
                        K--;
