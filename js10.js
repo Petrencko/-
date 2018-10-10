8a82e6f1bfda2af898b1bf5f1b01c328f651f41f
Petrencko <39306668+Petrencko@users.noreply.github.com>
2018-10-10 15:09:38 +0300

    Update js10.js

--- js10.js
+++ js10.js
@@ -16,7 +16,7 @@ function f1()
 {N=parseFloat($('t1').value);
  K=1;
  S=1;
-                while(K<N, S<=N)
+                while(K<N, S<N)
 	               S=S+K;	
 		       K=K+1;
                        
