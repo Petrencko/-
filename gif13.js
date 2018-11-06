13d22f5dec017a38bbedcef02e9c54f684e1d399
Petrencko <39306668+Petrencko@users.noreply.github.com>
2018-11-07 01:14:57 +0200

    Add files via upload

---
+++ gif13.js
@@ -0,0 +1,23 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; } 
+function f(){
+	
+	var t = setInterval("g()",2000);
+}
+function g()
+{var x1=window.getComputedStyle($('v1'), null);
+var y1=x1.zIndex;
+var y2=x2.zIndex;
+var y3=x3.zIndex;
+var x = y1
+$('v1').style.zIndex =y2;
+}
